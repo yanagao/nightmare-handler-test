7eccc3602f57164d365f5418a6096f500fe1a166
yanagao <guanyaoyaogaoya@sina.com>
2018-06-21 20:52:47 +0800

    update

---
+++ test/index.test.js
@@ -0,0 +1,10 @@
+// var test = require('../demo/demo1.js');
+var expect = require('chai').expect;
+
+console.log('ddddddd', test)
+
+// describe('加法函数的测试', function () {
+//     it('1 加 1 应该等于 2', function () {
+//         expect(recorderResult.args).to.be.equal('');
+//     });
+// });
