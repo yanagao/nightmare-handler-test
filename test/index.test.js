fed1c6b9c71243e1c168d2493d800e2ba3375533
yanagao <guanyaoyaogaoya@sina.com>
2018-06-22 17:38:38 +0800

    update

--- test/index.test.js
+++ test/index.test.js
@@ -1,10 +1,53 @@
 // var test = require('../demo/demo1.js');
 var expect = require('chai').expect;
+var Nightmare = require('nightmare');
 
-console.log('ddddddd', test)
+// var WebEventRecorder = require('../lib/tools/web-event-recorder').default;
+var WebEventRecorder = require('../lib').WebEventRecorder;
 
-// describe('加法函数的测试', function () {
-//     it('1 加 1 应该等于 2', function () {
-//         expect(recorderResult.args).to.be.equal('');
-//     });
-// });
+
+
+var recorderResult = [];
+
+
+describe('resize-image', function() {
+	it('hhhhhhhhhhh', function() {
+		// 更多选项可参考 https://github.com/segmentio/nightmare#nightmareoptions
+		var nightmare = Nightmare({
+			show: true
+		});
+		var recorder = new WebEventRecorder(nightmare);
+
+		// 执行
+		nightmare.goto('http://www.baidu.com')
+			.type('form[action*="/s"] [name=f]', 'nightmare')
+			.click('form[action*="/s"] [type=submit]')
+			.wait('#content_left')
+			.evaluate(function() {
+				return Array.from(document.querySelectorAll('#content_left .c-container'))
+					.map(function(item) {
+						return item.querySelector('.t').innerText;
+					});
+			})
+			.end()
+			.then(function(result) {
+				// console.log(result);
+				// console.log(recorder.toString());
+				recorderResult = JSON.parse(recorder.toString());
+				for (var i = 0; i < recorderResult.length; i++) {
+					if (recorderResult[i].args[3]) {
+						try{
+							expect(recorderResult[i].args[3]).to.have.string('https://www.baidu.com/')
+						} catch(e) {
+							console.log('dddd', i)
+						}
+					}
+					
+				}
+			})
+			.catch(function(error) {
+				console.error('Search failed:', error);
+			});
+	});
+});
+
