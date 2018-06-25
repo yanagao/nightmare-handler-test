84df862e66bfcfbf98201c1c42bd5ed6f77af91d
yanagao <guanyaoyaogaoya@sina.com>
2018-06-25 19:29:05 +0800

    update

--- test/index.test.js
+++ test/index.test.js
@@ -10,8 +10,8 @@ var WebEventRecorder = require('../lib').WebEventRecorder;
 var recorderResult = [];
 
 
-describe('resize-image', function() {
-	it('hhhhhhhhhhh', function() {
+describe('CDN资源', function() {
+	it('符合', function() {
 		// 更多选项可参考 https://github.com/segmentio/nightmare#nightmareoptions
 		var nightmare = Nightmare({
 			show: true
@@ -34,12 +34,23 @@ describe('resize-image', function() {
 				// console.log(result);
 				// console.log(recorder.toString());
 				recorderResult = JSON.parse(recorder.toString());
+				console.log(recorderResult[3])
+				// console.log(recorderResult[13])
 				for (var i = 0; i < recorderResult.length; i++) {
-					if (recorderResult[i].args[3]) {
-						try{
-							expect(recorderResult[i].args[3]).to.have.string('https://www.baidu.com/')
+					if (recorderResult[i].args[3]) { // CDN判断
+						try {
+							expect(recorderResult[i].args[3]).to.match(/pic\.url\.cn|qpic\.url\.cn/) // pic.url.cn和qpic.url.cn为CDN域名，p.qlogo.cn和p.qpic.cn为非CDN域名
 						} catch(e) {
-							console.log('dddd', i)
+							// console.log('第', i, '条为非CDN域名')
+						}
+					} else {
+						// console.log('第', i, '条为非CDN域名')
+					}
+					if (recorderResult[i].args[7] && (recorderResult[i].args[7]['content-type'][0].indexOf('image') !== -1)) { // 图片尺寸
+						try {
+							expect(parseInt(recorderResult[i].args[7]['content-length'][0])).to.be.at.most(80000)
+						} catch(e) {
+							console.log(recorderResult[i].args[3], '图片大小超限')
 						}
 					}
 					
